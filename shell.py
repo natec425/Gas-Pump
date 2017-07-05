331f78c592a2598ed8e1baaf8d4049e64df1ba9d
Nate Clark <natec425@gmail.com>
2017-07-05 09:37:52 -0500

    [Shell] Sell single gas type

--- shell.py
+++ shell.py
@@ -1,5 +1,11 @@
+GAS_PRICE = 2.00
+
+
 def main():
-    print('hello world')
+    print('Gas costs ${:.2f} per gallon.'.format(GAS_PRICE))
+    gallons = float(input('How many gallons of gas would you like to buy: '))
+    print('Thank you for your purchase. That will be ${:.2f}.'.format(gallons * GAS_PRICE))
+
 
 if __name__ == '__main__':
     main()
