23b09ba520a6963df2c98561a64824a59ef456cf
Nate Clark <natec425@gmail.com>
2017-07-05 09:51:57 -0500

    [Core] Add gas_price

---
+++ core.py
@@ -0,0 +1,21 @@
+def gas_price(gas_type):
+    '''String -> Float
+
+    Returns the price per gallon of the provided gas type.
+    If an invalid gas type is provided, None is returned.
+
+    >>> gas_price('Regular')
+    2.0
+    >>> gas_price('Medium')
+    2.5
+    >>> gas_price('Premium')
+    3.0
+    >>> gas_price('something else') is None
+    True
+    '''
+    if gas_type == 'Regular':
+        return 2.0
+    elif gas_type == 'Medium':
+        return 2.5
+    elif gas_type == 'Premium':
+        return 3.0
