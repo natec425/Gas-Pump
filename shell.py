43b9fe076f3e11f29f4338b25b372f0dbdc19b44
Nate Clark <natec425@gmail.com>
2017-07-05 09:48:33 -0500

    [Shell] Sell regular, medium, and premium gas

--- shell.py
+++ shell.py
@@ -1,10 +1,27 @@
-GAS_PRICE = 2.00
+REGULAR_PRICE = 2.00
+MEDIUM_PRICE = 2.50
+PREMIUM_PRICE = 3.00
 
 
 def main():
-    print('Gas costs ${:.2f} per gallon.'.format(GAS_PRICE))
+    print('Gas costs: Regular (${:.2f}), Medium (${:.2f}), Premium (${:.2f})'.format(
+        REGULAR_PRICE, MEDIUM_PRICE, PREMIUM_PRICE))
+
+    gas_type = input('Which type of gas would you like to buy: ')
+
+    if gas_type == 'Regular':
+        gas_price = REGULAR_PRICE
+    elif gas_type == 'Medium':
+        gas_price = MEDIUM_PRICE
+    elif gas_type == 'Premium':
+        gas_price = PREMIUM_PRICE
+    else:
+        print('Invalid gas type... Program exitting...')
+        exit()
+
     gallons = float(input('How many gallons of gas would you like to buy: '))
-    print('Thank you for your purchase. That will be ${:.2f}.'.format(gallons * GAS_PRICE))
+
+    print('Thank you for your purchase. That will be ${:.2f}.'.format(gallons * gas_price))
 
 
 if __name__ == '__main__':
